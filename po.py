27449b3eb670f4451cfe1d9d02910ca86757dcea
andrilchakraborty <theyungju@gmail.com>
2025-02-11 02:28:12 -0800

    Update po.py

--- po.py
+++ po.py
@@ -6,7 +6,7 @@ app.secret_key = "3f9c4e0c5fddba2dc14b89f3d16e3e2d1fc9db16c7637c1b5f8d6a76908fd8
 
 DISCORD_CLIENT_ID = "1337995257316380725"
 DISCORD_CLIENT_SECRET = "3FtwkBe01SsFZBqzwDNqVirsQNTiyhK9"
-REDIRECT_URI = "https://quiet-pzo9.vercel.app/callback"
+REDIRECT_URI = "https://quiet-pzo9-git-main-andrils-projects.vercel.app/callback"
 DISCORD_AUTH_URL = "https://discord.com/api/oauth2/authorize"
 DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token"
 DISCORD_API_URL = "https://discord.com/api/v10/users/@me"
